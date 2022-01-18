bfbd8e6ee38e1d77bb903fb413aae0154c2704a6
Iliana Meza <ilianameza530@gmail.com>
2022-01-18 10:37:48 +0000

    - test that the number input is not empty - add format documentation and color to .rspec

--- lib/Formatter/PhoneNumber/uk.rb
+++ lib/Formatter/PhoneNumber/uk.rb
@@ -1,15 +1,21 @@
 #TODO
-# no number is input
 # check length of the number (11-13 is acceptable)
 # check that only numbers and '+' are used in the string
 # check that one of the three formats applies to the number 
 # return the number with the proper +447 prefixed to the rest of the string
+# extract errors into their own classes
 
 module Formatter
     module PhoneNumber
         class UK
             def call(number)
-                number
+                no_number_input(number)
+            end
+
+            private
+
+            def no_number_input(number)
+                number.length == 0 ? raise("You must provide a phone number") : number
             end
         end
     end
--- spec/Formatter/PhoneNumber/uk_spec.rb
+++ spec/Formatter/PhoneNumber/uk_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Formatter::PhoneNumber::UK, "#format" do
             expect(subject.call(number)).to eq('+447356432176')
         end
     end
+
+    context "with an invalid format" do
+        it "raises an error when the string input is empty" do
+            number = ""
+            expect{subject.call(number)}.to raise_error(RuntimeError, "You must provide a phone number")
+        end
+    end
 end
